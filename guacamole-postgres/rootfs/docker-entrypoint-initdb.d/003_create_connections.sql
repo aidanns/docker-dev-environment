de6c87a3ea0e1e1346b57911ae059853db990391
Aidan Nagorcka-Smith <aidanns@gmail.com>
2022-01-01 22:08:00 +1100

    Set ownership correctly of home dir and creat VNC connection with correct port

--- guacamole-postgres/rootfs/docker-entrypoint-initdb.d/003_create_connections.sql
+++ guacamole-postgres/rootfs/docker-entrypoint-initdb.d/003_create_connections.sql
@@ -1,9 +1,9 @@
 -- Create connections
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('desktop-ssh', 'ssh');
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('desktop-vnc', 'vnc');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('SSH', 'ssh');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('VNC', 'vnc');
 
 -- Add parameters to the new connection
 INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', 'desktop');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'port', '22');
 INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', 'desktop');
-INSERT INTO guacamole_connection_parameter VALUES (2, 'port', '5900');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'port', '5901');
